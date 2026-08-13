4de2e8e1015c625c6ebe9cab952e1fd92476e670
Alex Childers <42629593+abchilders@users.noreply.github.com>
2026-08-14 11:00:14 +1200

    Create template for course notes

---
+++ notes template.py
@@ -0,0 +1,10 @@
+# [Module Title]
+# Example used in this lesson: [project theme, e.g. recipe scaler] 
+
+# TOPIC HEADING:
+# * topic 1 notes 
+#    * subtopic notes
+# * topic 2 notes
+
+# ---Topic---
+print("put code examples here, label heading with topic this is supposed to illustrate")
