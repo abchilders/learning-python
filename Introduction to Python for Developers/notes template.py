c4152d141e9d6f5df6e92d31cad7699b6ddbd005
Alex Childers <42629593+abchilders@users.noreply.github.com>
2026-08-17 09:40:47 +1200

    Make topic headings more visible in notes template

--- Introduction to Python for Developers/notes template.py
+++ Introduction to Python for Developers/notes template.py
@@ -1,12 +1,10 @@
 # [Module Title]
 # Example used in this lesson: [project theme, e.g. recipe scaler] 
 
-# ==============================================================================
-# TOPIC HEADING 
-# ==============================================================================
+# TOPIC HEADING:
 # * topic 1 notes 
 #    * subtopic notes
 # * topic 2 notes
 
-# ------Examples: Topic------
+# ---Topic---
 print("put code examples here, label heading with topic this is supposed to illustrate")
